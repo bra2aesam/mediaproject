aeae66285c1fc03fef4125f1458e5f2614a5d98d
mohammadalmobayed <mohammadalmobayed4@gmail.com>
2023-02-27 23:09:41 +0300

    11

--- src/apis/GroupService.js
+++ src/apis/GroupService.js
@@ -3,7 +3,10 @@ import axios from 'axios';
 const USER_API_URL = "http://localhost/mediasocial/backend/groupcrud";
 
 class GroupService {
-
+   
+    getSingleGroup(group){
+        return axios.post(`http://localhost/mediasocial/backend/group/index.php`, group);
+    }
     createGroup(group){
         return axios.post(`${USER_API_URL}/create.php`, group);
     }
